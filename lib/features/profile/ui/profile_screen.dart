dc5f74866e0eb0cc31572f02e65b2396d500d56a
Taha Hamada <93037873+taha2901@users.noreply.github.com>
2024-09-28 16:54:55 +0300

    - profile shimmer loading is done.

--- lib/features/profile/ui/profile_screen.dart
+++ lib/features/profile/ui/profile_screen.dart
@@ -7,12 +7,13 @@ import 'package:snap_shop/core/helpers/shared_pref_helper.dart';
 import 'package:snap_shop/core/helpers/spacing.dart';
 import 'package:snap_shop/core/routings/routers.dart';
 import 'package:snap_shop/core/theming/styles.dart';
-import 'package:snap_shop/features/profile/data/model/profile_model/profile_model.dart';
 import 'package:snap_shop/features/profile/logic/profile_cubit.dart';
+import 'package:snap_shop/features/profile/ui/widget/profile_user_data_shimmer_laoding.dart';
 
 class ProfileScreen extends StatelessWidget {
-  final ProfileModel profileModel;
-  const ProfileScreen({super.key, required this.profileModel});
+  // final ProfileModel profileModel;
+
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +22,18 @@ class ProfileScreen extends StatelessWidget {
         state.whenOrNull(
           profileSuccess: (profileModel) {
             ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Success'),
-              ),
+              const SnackBar(content: Text('Success')),
             );
           },
           profileLoading: () {
             ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Loading...'),
-              ),
+              const SnackBar(content: Text('Loading...')),
             );
           },
           profileError: (error) {
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-              content: Text(error.apiErrorModel.message ?? ''),
-            ));
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(error.apiErrorModel.message ?? '')),
+            );
           },
         );
       },
@@ -58,115 +55,72 @@ class ProfileScreen extends StatelessWidget {
                       ),
                     ),
                     verticalSpace(16.0),
-                    Card(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0),
-                          color: Colors.grey.shade200,
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 16.w, vertical: 16.h),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                "${profileModel.data?.name}",
-                                style: TextStyles.font16BlackBold,
+                    // هنا نقوم بإظهار الـ Card حسب حالة التحميل
+                    BlocBuilder<ProfileCubit, ProfileState>(
+                      builder: (context, state) {
+                        if (state is ProfileLoading) {
+                          return ProfileUserDataShimmerLoading();
+                        } else if (state is ProfileSuccess) {
+                          return Card(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                color: Colors.grey.shade200,
                               ),
-                              verticalSpace(8),
-                              Row(
-                                children: [
-                                  Text(
-                                    "${profileModel.data?.email}",
-                                    style: TextStyles.font14LightGreyRegular,
-                                  ),
-                                  const Spacer(),
-                                  GestureDetector(
-                                      child: Text(
-                                    "Edit",
-                                    style: TextStyles.font16MainColorMedium,
-                                  )),
-                                ],
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 16.w, vertical: 16.h),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "${state.profileModel!.data?.name}",
+                                      style: TextStyles.font16BlackBold,
+                                    ),
+                                    verticalSpace(8),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          "${state.profileModel!.data?.email}",
+                                          style:
+                                              TextStyles.font14LightGreyRegular,
+                                        ),
+                                        const Spacer(),
+                                        GestureDetector(
+                                          child: Text(
+                                            "Edit",
+                                            style: TextStyles
+                                                .font16MainColorMedium,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    verticalSpace(8),
+                                    Text(
+                                      "${state.profileModel!.data?.phone}",
+                                      style: TextStyles.font14LightGreyRegular,
+                                    ),
+                                  ],
+                                ),
                               ),
-                              verticalSpace(8),
-                              Text(
-                                "${profileModel.data?.phone}",
-                                style: TextStyles.font14LightGreyRegular,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                            ),
+                          );
+                        } else {
+                          return const SizedBox
+                              .shrink(); // إرجاع مساحة فارغة في حالات أخرى
+                        }
+                      },
                     ),
                     verticalSpace(16.0),
-                    Card(
-                      child: ListTile(
-                        leading: const Icon(Iconsax.timer_1),
-                        title: const Text("Change Password"),
-                        onTap: () {
-                          context.pushNamed(Routers.changePassword);
-                        },
-                      ),
-                    ),
-                    Card(
-                      child: ListTile(
-                        leading: const Icon(Iconsax.information),
-                        trailing: IconButton(
-                            onPressed: () {}, icon: const Icon(Iconsax.edit)),
-                        title: const Text("Update Profile"),
-                        onTap: () {
-                          context.pushNamed(Routers.updateProfile);
-                        },
-                      ),
-                    ),
-                    Card(
-                      child: ListTile(
-                        trailing: IconButton(
-                          onPressed: () {
-                            context.pushNamed(Routers.addAddress);
-                          },
-                          icon: const Icon(Icons.arrow_forward_ios),
-                        ),
-                        title: const Text("Address"),
-                        onTap: () {
-                          context.pushNamed(Routers.address);
-                        },
-                      ),
-                    ),
-                    Card(
-                      child: ListTile(
-                        trailing: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.arrow_forward_ios),
-                        ),
-                        title: const Text("wishlist"),
-                        onTap: () {
-                          context.pushNamed(Routers.favourite);
-                        },
-                      ),
-                    ),
+                    // باقي الأزرار
+                    _buildListTile(
+                        context, "Change Password", Routers.changePassword),
+                    _buildListTile(
+                        context, "Update Profile", Routers.updateProfile),
+                    _buildListTile(context, "Address", Routers.address),
+                    _buildListTile(context, "Wishlist", Routers.favourite),
                     verticalSpace(16.0),
-                    // زر تسجيل الخروج
-                    Card(
-                      color: Colors.red,
-                      child: ListTile(
-                        leading: const Icon(Icons.logout, color: Colors.white),
-                        title: const Text(
-                          "Logout",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onTap: () async {
-                          // حذف البيانات المخزنة
-                          await SharedPrefHelper.clearAllData();
-                          await SharedPrefHelper.clearAllSecuredData();
-
-                          // توجيه المستخدم إلى صفحة تسجيل الدخول
-                          Navigator.pushNamedAndRemoveUntil(
-                              context, Routers.login, (route) => false);
-                        },
-                      ),
-                    ),
+                    _buildLogoutTile(context),
                   ],
                 ),
               ),
@@ -176,4 +130,37 @@ class ProfileScreen extends StatelessWidget {
       },
     );
   }
+
+  Widget _buildListTile(BuildContext context, String title, String route) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Iconsax.timer_1),
+        title: Text(title),
+        onTap: () {
+          context.pushNamed(route);
+        },
+      ),
+    );
+  }
+
+  Widget _buildLogoutTile(BuildContext context) {
+    return Card(
+      color: Colors.red,
+      child: ListTile(
+        leading: const Icon(Icons.logout, color: Colors.white),
+        title: const Text(
+          "Logout",
+          style: TextStyle(color: Colors.white),
+        ),
+        onTap: () async {
+          await SharedPrefHelper.clearAllData();
+          await SharedPrefHelper.clearAllSecuredData();
+          Navigator.pushNamedAndRemoveUntil(
+              context, Routers.login, (route) => false);
+        },
+      ),
+    );
+  }
 }
+
+
--- lib/features/profile/ui/widget/profile_bloc_builder.dart
+++ lib/features/profile/ui/widget/profile_bloc_builder.dart
@@ -1,47 +1,46 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:snap_shop/core/networking/api_error_handler.dart';
-import 'package:snap_shop/features/profile/logic/profile_cubit.dart';
-import 'package:snap_shop/features/profile/ui/profile_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:snap_shop/core/networking/api_error_handler.dart';
+// import 'package:snap_shop/features/profile/logic/profile_cubit.dart';
+// import 'package:snap_shop/features/profile/ui/profile_screen.dart';
 
-class ProfileBlocBuilder extends StatelessWidget {
-  const ProfileBlocBuilder({
-    super.key,
-  });
+// class ProfileBlocBuilder extends StatelessWidget {
+//   const ProfileBlocBuilder({
+//     super.key,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ProfileCubit, ProfileState>(
-        buildWhen: (previous, current) =>
-            current is ProfileLoading ||
-            current is ProfileSuccess ||
-            current is ProfileError,
-        builder: (context, state) {
-          return state.maybeWhen(
-            profileLoading: () => setUpLoading(),
-            profileSuccess: (profileData) {
-              return setUpSuccess(profileData);
-            },
-            profileError: (error) => setupError(error),
-            orElse: () => const SizedBox.shrink(),
-          );
-        });
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return BlocBuilder<ProfileCubit, ProfileState>(
+//         buildWhen: (previous, current) =>
+//             current is ProfileLoading ||
+//             current is ProfileSuccess ||
+//             current is ProfileError,
+//         builder: (context, state) {
+//           return state.maybeWhen(
+//             profileLoading: () => setUpLoading(),
+//             profileSuccess: (profileData) {
+//               return setUpSuccess(profileData);
+//             },
+//             profileError: (error) => setupError(error),
+//             orElse: () => const SizedBox.shrink(),
+//           );
+//         });
+//   }
 
-  Widget setupError(ErrorHandler error) {
-    return Center(
-        child: Scaffold(
-            body:
-                Center(child: Text('Error: ${error.apiErrorModel.message}'))));
-  }
+//   Widget setupError(ErrorHandler error) {
+//     return Center(
+//         child: Scaffold(
+//             body:
+//                 Center(child: Text('Error: ${error.apiErrorModel.message}'))));
+//   }
 
-  Widget setUpSuccess(profileData) {
-    return ProfileScreen(
-      profileModel: profileData,
-    );
-  }
+//   Widget setUpSuccess(profileData) {
+//     return ProfileScreen(
+//     );
+//   }
 
-  Widget setUpLoading() {
-    return const Scaffold(body: Center(child: CircularProgressIndicator()));
-  }
-}
+//   Widget setUpLoading() {
+//     return const Scaffold(body: Center(child: CircularProgressIndicator()));
+//   }
+// }
