d869072e54c93e9dc6234b4036b57280166f227e
Taha Hamada <93037873+taha2901@users.noreply.github.com>
2024-10-06 12:22:04 +0300

    - لسه في ايرور

---
+++ lib/core/helpers/api_keys.dart
@@ -0,0 +1,6 @@
+class ApiKeys {
+  static String secretkey =
+      'sk_test_51Q4r3VIoXuYanf6KYs0HjE2rjTz79B0R4XVrwcvcmehyobA79BhQppeiI4E6tPESGDDH03gygnC9ugYNgjBUYojf00yTl2joDw';
+  static String publishableKey =
+      'pk_test_51Q4r3VIoXuYanf6KSRtJOXZQ9zkZThGIstp2EtKzxsyv517nZDo6r7yl9RTWUvGqXW6vPLkMIASAydtR32J69ILe00e596uuyM';
+}
